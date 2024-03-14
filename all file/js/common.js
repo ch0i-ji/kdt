e8c72ee04e2dd69ae99d6b0a4e6e397d0a843236
mowha <ch0i_jiyoun@naver.com>
2024-03-14 14:05:41 +0900

    swiper적용
    1

---
+++ all file/js/common.js
@@ -0,0 +1,12 @@
+let mainSlide = new Swiper(".mainSlide", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+  },
+});
